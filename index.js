d036d9c9d4dff6232ca0257c6ae6f04ad2c2c0f6
Jesus Alejandro Avendaño Peña <javendano13@alumnos.uaq.mx>
2025-03-22 20:40:22 -0600

    Ejercicio 7

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const pokemon = require('./routes/pokemon')
  PUT - modificar un recurso
  DELETE - borrar un recurso
 */
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,10 +18,14 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res, next) => {
-    return res.status(200).send("Bienvenido al pokedex")
+    return res.status(200).json({ code: 1, message: "Bienvenido al Pokedex" });
 });
 
 app.use("/pokemon", pokemon);
+
+app.use((req, res, next) => {
+    return res.status(404).json({ code: 404, message: "URL no encontrada" });
+})
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log('Server is running');
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -9,20 +9,27 @@ pokemon.post("/", (req, res, next) =>{
 
 pokemon.get("/", async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code:1, message: pkmn});
     
 
 });
+
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id;
-    const [pkmn] = await db.query("SELECT * FROM pokemon WHERE id = ?", [id]);
-
-    return (pkmn.length > 0) ? res.status(200).json(pkmn[0]) : res.status(404).send("Pokemon no encontrado");
+    if (id >= 1 && id <= 722) {
+        const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id=" + id + ";");
+        return res.status(200).json({ code: 1, message: pkmn });
+    }
+    return res.status(404).send({ code: 404, message: "Pokemon no encontrado" });
 });
+
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const name = req.params.name;
-    const pkmn = await db.query("SELECT * FROM pokemon WHERE name = ?", [name]);
-    return (pkmn.length > 0) ? res.status(200).json(pkmn) : res.status(404).send("Pokemon no encontrado");
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name='" + name + "';");
+    if (pkmn.length > 0) {
+        return res.status(200).json({ code: 1, message: pkmn });
+    }
+    return res.status(404).send("Pokemon no encontrado");
 });
 
 
