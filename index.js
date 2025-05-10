5b0f733655aad1aa5f39efd154f7e937a6d054d1
Jesus Alejandro Avendaño Peña <javendano13@alumnos.uaq.mx>
2025-05-10 14:05:19 -0600

    Conexión con la UI

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// Dependecies
 const express = require('express');
 const morgan = require("morgan")
 const app = express();
+//Routes
 const pokemon = require('./routes/pokemon')
 const user = require('./routes/user')
+//Middleware
 const auth = require('./middleware/auth')
 const notFound = require('./middleware/notFound')
 const index = require('./middleware/index')
-
+const cors = require('./middleware/cors')
 /*
  GET - Obtener recursos
  POST - almacenar/crear recursos
@@ -14,7 +17,7 @@ const index = require('./middleware/index')
  PUT - modificar un recurso
  DELETE - borrar un recurso
 */
-
+app.use(cors)
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/", index);
-
 app.use("/user", user);
 app.use(auth);
 app.use("/pokemon", pokemon);
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const user = express.Router();
 const db = require('../config/database');
 const jwt = require('jsonwebtoken');
 
-user.post("/", async (req, res, next) => {
+user.post("/signin", async (req, res, next) => {
     const { user_name, user_mail, user_password } = req.body;
 
     if (user_name && user_mail && user_password) {
@@ -34,7 +34,7 @@ user.post("/login", async (req, res, next) => {
             }, "debugkey")
             return res.status(200).json({ code: 200, token});
         }else{
-            return res.status(401).json({ code: 401, message: "Usuario no encontrado" });
+            return res.status(200).json({ code: 200, message: "Usuario no encontrado" });
         }
     }
     return res.status(500).json({ code: 500, message: "Campos incompletos" });
